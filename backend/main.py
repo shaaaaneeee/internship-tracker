c718581f17348e31b9fee875e6b762512a3fa315
shaaaaneeee <shaneslee18@icloud.com>
2026-03-02 16:54:59 +0800

    fixed typo AGAIN

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://internship-tracker-shane.vercel.app/signup",
+        "https://internship-tracker-shane.vercel.app/",
         "https://*.vercel.app"
     ],
     allow_credentials=True,
