1c97d17daa3358c2905b387e98882f52ac5a8163
shaaaaneeee <shaneslee18@icloud.com>
2026-03-02 16:52:54 +0800

    fixed typo in CORS code

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://internship-tracker.vercel.app",
+        "https://internship-tracker-shane.vercel.app/signup",
         "https://*.vercel.app"
     ],
     allow_credentials=True,
