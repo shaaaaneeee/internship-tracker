4b77bdc1e0e7f5bdadf9b338fb526d68d9d12a00
shaaaaneeee <shaneslee18@icloud.com>
2026-03-03 01:07:14 +0800

    Fixed Axios connection to backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://internship-tracker-api-fqn6.onrender.com',
 })
 
 api.interceptors.request.use((config) => {
